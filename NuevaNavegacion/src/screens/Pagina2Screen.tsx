df26762a3b3515b7bd18236fe5638541c4f363d7
JorgeVC <test@test.com>
2023-10-02 17:24:55 -0500

    creacion stacknavigator

---
+++ NuevaNavegacion/src/screens/Pagina2Screen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export const Pagina2Screen = () => {
+    return (
+        <View>
+            <Text>
+                Pagina2Screen
+            </Text>
+        </View>
+    )
+}
